bfa01b5a90cc27090963d4110868dac47d2e9bd2
srk5309 <53816542+srk5309@users.noreply.github.com>
2021-06-15 22:24:49 +0530

    Add files via upload

---
+++ Continueas.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text,StyleSheet,Image,ImageBackground,TouchableOpacity,Dimensions,TextInput } from 'react-native'
+var {height} = Dimensions.get('window')
+var {width} = Dimensions.get('window')
+const Continueas = ({navigation})=> {
+    return (
+        <View>
+            <Text>Continue</Text>
+        </View>
+    )
+}
+
+export default Continueas
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Useraccount.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, Text,Dimensions,Image,TextInput,StyleSheet } from 'react-native'
+var {height} = Dimensions.get('window')
+var {width} = Dimensions.get('window')
+const Useraccount = ({navigation})=> {
+    return (
+        <View>
+            <Text>Useraccount</Text>
+        </View>
+    )
+}
+
+export default Useraccount
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
